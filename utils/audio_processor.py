a22971d897455ac10dfc5022d2777fe047d19203
Smita Saroj <smitasaroj9594@gmail.com>
2026-06-25 16:49:41 +0530

    Fix YouTube downloader

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -2,25 +2,57 @@ import yt_dlp
 from pydub import AudioSegment
 import os
 
-DOWNLOAD_DIR = 'downloades'
+DOWNLOAD_DIR = 'downloads'
 os.makedirs(DOWNLOAD_DIR,exist_ok = True)
 
 def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
+    # ydl_opts = {
+    #     "format": "bestaudio/best",
+    #     "outtmpl": output_path,
+    #     "postprocessors": [
+    #         {
+    #             "key": "FFmpegExtractAudio",
+    #             "preferredcodec": "wav",
+    #             "preferredquality": "192",
+    #         }
+    #     ],
+    #     "quiet": True,
+    # }
+
     ydl_opts = {
-        "format": "bestaudio/best",
-        "outtmpl": output_path,
-        "postprocessors": [
-            {
-                "key": "FFmpegExtractAudio",
-                "preferredcodec": "wav",
-                "preferredquality": "192",
-            }
-        ],
-        "quiet": True,
-    }
+    "format": "bestaudio/best",
+    "outtmpl": output_path,
+    "noplaylist": True,
+    "quiet": True,
+    "no_warnings": True,
+    "geo_bypass": True,
+    "extractor_args": {
+        "youtube": {
+            "player_client": ["android"]
+        }
+    },
+    "http_headers": {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/137.0.0.0 Safari/537.36"
+        )
+    },
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "wav",
+            "preferredquality": "192",
+        }
+    ],
+}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
+        # info = ydl.extract_info(url, download=True)
+        try:
+          info = ydl.extract_info(url, download=True)
+        except Exception as e:
+               raise RuntimeError(f"YouTube download failed: {e}")
         filename = ydl.prepare_filename(info).replace(".webm", ".wav").replace(".m4a", ".wav")
     return filename
 
